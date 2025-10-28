7a5dd0b16a89b9bbff1055bc276ec84adde491b3
lihloway <80195957+lihloway@users.noreply.github.com>
2025-10-28 12:42:45 +0800

    Fix testcase

--- src/test/java/seedu/address/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -3,41 +3,30 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
 
 class ExportCommandTest {
 
     @TempDir
     public Path tempDir; // JUnit provides a temporary directory
 
-    private Model model;
-
-    @BeforeEach
-    void setUp() throws IOException {
-        // Create a temporary source file to simulate the existing address book
-        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-    }
+    private Model model = new ModelManager();
 
     @Test
     void execute_validFilePath_success() throws Exception {
-        Path exportPath = tempDir.resolve("export.json");
 
         // Check that the file was copied
-        assertCommandSuccess(new ExportCommand(exportPath.toString()), model,
-                String.format(ExportCommand.MESSAGE_SUCCESS, exportPath), model);
+        assertCommandSuccess(new ExportCommand(tempDir.toString()), model,
+                String.format(ExportCommand.MESSAGE_SUCCESS, tempDir.toString()) + "\\" + ExportCommand.DEFAULT_FILE, model);
         //assertTrue(Files.exists(exportPath));
     }
 
@@ -49,7 +38,7 @@ class ExportCommandTest {
         ExportCommand command = new ExportCommand(exportDir.toString());
         CommandResult result = command.execute(model);
 
-        Path expectedFile = exportDir.resolve("exported_addressbook.json");
+        Path expectedFile = exportDir.resolve(ExportCommand.DEFAULT_FILE);
         assertTrue(Files.exists(expectedFile));
         //assertEquals(String.format(ExportCommand.MESSAGE_SUCCESS, expectedFile), result.getFeedbackToUser());
     }
